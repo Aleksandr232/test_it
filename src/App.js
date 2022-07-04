89182eabacd59c9004b56278ab9f78b319757ecc
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-04 14:40:40 +0300

    test_it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ap_fon'>
+      <div className='grid_a'>
+        <a href="">БИЛЕТЫ И АБОНЕМЕНТЫ</a>
+        <a href="">Как купить?</a>
+        <a href="">Правила</a>
+        <a href="">Возврат билетов</a>
+      </div>
+      <div className='orbit'>
+          <div className='mini_orbit'></div>
+      </div>
     </div>
   );
 }
